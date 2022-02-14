e341e58bcd7a369436c38f531efb3e4ff5c15983
José Cabral <dev.josecabral@gmail.com>
2022-02-14 10:55:19 +0000

    feat: add initial component structure and theme configurations

--- tailwind-nft-preview/tailwind.config.js
+++ tailwind-nft-preview/tailwind.config.js
@@ -3,7 +3,16 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    fontFamily: {
+        'outfit': ['Outfit', 'sans-serif']
+    },
+    colors: {
+        'soft-blue': '#8bacda',
+        'cyan': '#00fff7',
+        'main-bg': '#0d192b',
+        'card-bg': '#14253d',
+        'line': '#2f415b',
+    },
   },
   plugins: [],
 }
