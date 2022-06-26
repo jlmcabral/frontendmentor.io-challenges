dcb4c3bf071856737453d6c828f51712f22795ff
jlmcabral <dev.josecabral@gmail.com>
2022-06-26 15:45:14 +0100

    feat: add hover states

--- tailwind-nft-preview/src/app/app.component.ts
+++ tailwind-nft-preview/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tailwind-nft-preview';
+  cardTitle: string = "Equilibrium #3429";
+  cardImageUrl: string = "../assets/images/image-equilibrium.jpg";
+  creatorImgUrl: string = "../assets/images/image-avatar.png";
+  creatorName: string = "Jules Wyvern";
+  price: number = 0.041;
+  daysLeft: number = 3;
 }
