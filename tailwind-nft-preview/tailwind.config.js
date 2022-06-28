cf86d24ab0eafe1e493b4010936fb80db4b8306d
jlmcabral <dev.josecabral@gmail.com>
2022-06-28 18:07:54 +0100

    style: add soft-transitions on hover and refactor tailwind.config.js

--- tailwind-nft-preview/tailwind.config.js
+++ tailwind-nft-preview/tailwind.config.js
@@ -3,17 +3,19 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    fontFamily: {
-        'outfit': ['Outfit', 'sans-serif']
-    },
-    colors: {
-        'soft-blue': '#8bacda',
-        'cyan': '#00fff7',
-        'main-bg': '#0d192b',
-        'card-bg': '#14253d',
-        'line': '#2f415b',
-        'white': '#ffffff',
-    },
+    extend: {
+        fontFamily: {
+            'outfit': ['Outfit', 'sans-serif']
+        },
+        colors: {
+            'soft-blue': '#8bacda',
+            'cyan': '#00fff7',
+            'main-bg': '#0d192b',
+            'card-bg': '#14253d',
+            'line': '#2f415b',
+            'white': '#ffffff',
+        },
+    }
   },
   plugins: [],
 }
